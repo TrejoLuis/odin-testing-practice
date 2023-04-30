9bdd84300a01db26b2f190203262119e51993187
Luis Trejo <trejorias@gmail.com>
2023-04-30 09:58:12 -0600

    test(reverseString): check the input to be a string

--- src/test/reverseString.test.js
+++ src/test/reverseString.test.js
@@ -3,3 +3,11 @@ import  {reverseString} from '../reverseString.js'
 test('should be a function', () => {
   expect(typeof reverseString).toBe('function')})
 
+test('should accept only string type', () => {
+  expect(() => reverseString(123)).toThrow('The input value must be a string')
+  expect(() => reverseString(['a', 'b', 'c'])).toThrow('The input value must be a string')
+  expect(() => reverseString({foo: 'bar', baz: 'bundy'})).toThrow('The input value must be a string')
+  expect(() => reverseString(true)).toThrow('The input value must be a string')
+  expect(() => reverseString(Symbol('foo'))).toThrow('The input value must be a string')
+})
+
