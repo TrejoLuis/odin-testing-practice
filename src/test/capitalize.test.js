26ca08b70fe98cd1d29ae315d2af6defc00736a1
Luis Trejo <trejorias@gmail.com>
2023-04-29 20:18:30 -0600

    a

--- src/test/capitalize.test.js
+++ src/test/capitalize.test.js
@@ -4,3 +4,8 @@ import {capitalize} from '../capitalize.js'
 test('capitalize is a function', () => {
   expect(typeof capitalize).toBe('function')})
 
+test('returns a string', () => {
+  expect(typeof capitalize('lorem')).toBe('string')})
+
+test('only accepts string value as parameter', () => {
+  expect(() => capitalize(123).toThrow('The value must be a string'))})
