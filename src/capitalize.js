d4a4dd47c979b6f41229eaa342db37454342856d
Luis Trejo <trejorias@gmail.com>
2023-04-29 21:37:18 -0600

    feat(capitalize): add bulk unit test and working function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,11 +1,12 @@
 //A function that takes a string and returns it with the first character capitalized
 const capitalize = (str) => {
   if(typeof str !== 'string') throw new Error('The value must be a string')
-  return str
+  if(str.match(/^( )+$/)) throw new Error('The string only contains white spaces')
+  if(str.match(/^[^a-z|A-Z]/)) throw new Error('The first character must be [a-z]')
+  const output = str.slice(0,1).toUpperCase()+str.slice(1)
+  return output 
 }
 
 export{
   capitalize
 }
-
-
--- src/test/capitalize.test.js
+++ src/test/capitalize.test.js
@@ -9,3 +9,23 @@ test('returns a string', () => {
 
 test('only accepts string value as parameter', () => {
   expect(() => capitalize(123).toThrow('The value must be a string'))})
+
+test('should avoid enter only white spaces', () => {
+  expect(() => capitalize('   ')).toThrow('The string only contains white spaces')})
+
+test('the first character must be [a-z]', () => {
+  expect(() => capitalize(' 3 $')).toThrow('The first character must be [a-z]')
+  expect(() => capitalize('  abc')).toThrow('The first character must be [a-z]')
+  expect(() => capitalize('.')).toThrow('The first character must be [a-z]')
+  expect(() => capitalize('  hello')).toThrow('The first character must be [a-z]')
+})
+
+test('capitalize first character', () => {
+  expect(capitalize('hello')).toBe('Hello')
+})
+
+test("capitalize first character even if it's a single character", () => {
+  expect(capitalize('g')).toBe('G')
+  expect(capitalize('A')).toBe('A')
+})
+
