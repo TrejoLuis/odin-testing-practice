3e02d41674d4734ea6d3de51db862415e44621b4
Luis Trejo <trejorias@gmail.com>
2023-04-29 20:17:55 -0600

    feat(capitalize): add input checker

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,7 +1,11 @@
+//A function that takes a string and returns it with the first character capitalized
 const capitalize = (str) => {
+  if(typeof str !== 'string') throw new Error('The value must be a string')
   return str
 }
 
 export{
   capitalize
 }
+
+
