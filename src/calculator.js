c6a2e2308160ec45617322878cd0d47c82b472fd
Luis Trejo <trejorias@gmail.com>
2023-05-01 15:54:53 -0600

    feat(calculator): add parameters validation

---
+++ src/calculator.js
@@ -0,0 +1,37 @@
+// A calculator object that contains functions for the basic
+// operations: add, subtract, divide, and multiply.
+// Each of these functions should take two numbers and return the correct calculation.
+const calculator = (() => {
+  const errorMsgValQuan = 'only two values must be provided'
+  const errorMsgValNotNumber = 'values provided must be number'
+
+  const add = (x, y, ...more) => {
+    if(more.length > 0 || typeof x == 'undefined' || typeof y == 'undefined') throw new Error(errorMsgValQuan)
+    if(typeof x !== 'number' || typeof y !== 'number'
+      || Number.isNaN(x) || Number.isNaN(y)) throw new Error(errorMsgValNotNumber)
+  }
+  const subtract = (x, y, ...more) => {
+    if(more.length > 0 || typeof x == 'undefined' || typeof y == 'undefined') throw new Error(errorMsgValQuan)
+    if(typeof x !== 'number' || typeof y !== 'number'
+      || Number.isNaN(x) || Number.isNaN(y)) throw new Error(errorMsgValNotNumber)
+  }
+  const divide = (x, y, ...more) => {
+    if(more.length > 0 || typeof x == 'undefined' || typeof y == 'undefined') throw new Error(errorMsgValQuan)
+    if(typeof x !== 'number' || typeof y !== 'number'
+      || Number.isNaN(x) || Number.isNaN(y)) throw new Error(errorMsgValNotNumber)
+  }
+  const multiply = (x, y, ...more) => {
+    if(more.length > 0 || typeof x == 'undefined' || typeof y == 'undefined') throw new Error(errorMsgValQuan)
+    if(typeof x !== 'number' || typeof y !== 'number'
+      || Number.isNaN(x) || Number.isNaN(y)) throw new Error(errorMsgValNotNumber)
+  }
+  
+  return{
+    add,
+    subtract,
+    divide,
+    multiply
+  }
+})()
+
+export {calculator}
