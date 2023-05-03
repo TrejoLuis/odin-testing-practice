99e95f91e4f1107bf7fbff0a4f478f377316311c
Luis Trejo <trejorias@gmail.com>
2023-05-03 13:11:25 -0600

    feat(cipher): add encrypt scope to encrypt string based on given shift, even works if the index is overlaping

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,9 +1,28 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+const map = new Map()
+for(let i=0; i<26; i++){
+  map.set(alphabet[i], i)
+}
+
 export const caesarCipher = (str, shift,...rest) => {
   const errMsgNParams = 'there must be only two values'
   const errMsgNoStr = 'the first value must be a string'
   const errMsgNoNum = 'the second value must be a number'
+
   if(rest.length > 0 || typeof str == 'undefined' || typeof shift == 'undefined') throw new Error(errMsgNParams)
   if(typeof str !== 'string') throw new Error(errMsgNoStr)
   if(typeof shift !== 'number' || Number.isNaN(shift)) throw new Error(errMsgNoNum)
+
+  let output = ''
+  //encrypting
+  for(let i=0; i<str.length; i++){
+    if(str[i].match(/[a-zA-Z]/)){
+      //rolling over the index both forward and backwards
+      let alphabetIndx = (map.get(str[i].toLowerCase()) + shift + 26)% 26
+      output += alphabet[alphabetIndx]
+    } else output += str[i]
+  }
+
+  return output 
   
 }
